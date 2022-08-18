19cec78d503714249a171f6ca812c7de091f16ae
ofsand <zesoufian@gmail.com>
2022-08-18 22:57:37 +0100

    Add routes to eshop

--- apps/eshop/src/app/app.module.ts
+++ apps/eshop/src/app/app.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'products',
+    component: ProductListComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -11,7 +23,7 @@ import { ProductListComponent } from './pages/product-list/product-list.componen
     HomePageComponent,
     ProductListComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
