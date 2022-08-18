50ce562d179e68311329829205d96bc866fa59b8
ofsand <zesoufian@gmail.com>
2022-08-18 16:03:41 +0100

    Project init

---
+++ apps/admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ecommerce-brands-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'admin';
+}
