00fe9eb6102a57e652e677fc6bb91b0d1bcf094e
ofsand <zesoufian@gmail.com>
2022-08-19 14:47:26 +0100

    Add dashboard component

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
