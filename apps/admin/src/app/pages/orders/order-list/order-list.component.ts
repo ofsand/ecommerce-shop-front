6ec9fd0d4ac5fd4f5e8e78aed20cb0a160f473c1
ofsand <zesoufian@gmail.com>
2022-08-25 23:26:14 +0100

    Add Delete Order functionnality on orders list table

--- apps/admin/src/app/pages/orders/order-list/order-list.component.ts
+++ apps/admin/src/app/pages/orders/order-list/order-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Order, OrdersService } from '@ecommerce-brands/orders';
+import { ConfirmationService, ConfirmEventType, MessageService } from 'primeng/api';
 import { STATUS } from '../order.status';
 
 
@@ -18,7 +19,9 @@ export class OrderListComponent implements OnInit {
 
   constructor(
     private orderService: OrdersService,
-    private router: Router
+    private router: Router,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService
   ) { }
 
   ngOnInit(): void {
@@ -33,6 +36,34 @@ export class OrderListComponent implements OnInit {
     this.orderService.getOrders().subscribe(orders => {
       this.orders = orders;
     })
+  }
+
+
+  deleteOrder(orderId: string) {
+    this.confirmationService.confirm({
+      message: 'Do you want to delete this category?',
+      header: 'Delete Category',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.orderService.deleteOrder(orderId).subscribe(
+          ()=> {
+          this._getOrders();
+                this.messageService.add({
+                  severity:'success', 
+                  summary:'Success', 
+                  detail:`this Order is been deleted successfully`
+                });
+          },
+          () => {
+                this.messageService.add({
+                  severity:'error', 
+                  summary:'Error', 
+                  detail:`Order is not deleted`
+                });
+        })
+      }
+  });
+    
   }
 
 }
