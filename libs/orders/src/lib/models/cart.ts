0db44827cdeb7844c9fd0a485ca9590fd068341d
ofsand <zesoufian@gmail.com>
2022-08-29 21:57:41 +0100

    Add Cart service and Model and Initialize the cart local storage

---
+++ libs/orders/src/lib/models/cart.ts
@@ -0,0 +1,8 @@
+export class Cart {
+    items?: CartItem[];
+}
+
+export class CartItem {
+    productId?: string;
+    quantity?: number;
+}
---
+++ libs/orders/src/lib/services/cart-service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor() { }
+
+  initCartLocalStorage() {
+    const initialCart = {
+      items: []
+    }
+    const initialCartJson = JSON.stringify(initialCart)
+    localStorage.setItem('cart', initialCartJson);
+  }
+}
