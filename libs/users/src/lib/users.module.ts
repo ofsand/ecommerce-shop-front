af625eb14757b58a67c54359ad1192660f764d90
ofsand <zesoufian@gmail.com>
2022-09-03 16:05:11 +0100

    Add login component

--- libs/users/src/lib/users.module.ts
+++ libs/users/src/lib/users.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-export const usersRoutes: Route[] = [];
+export const usersRoutes: Route[] = [
+  {
+    path: '/login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule.forChild(usersRoutes), CommonModule, RouterModule],
+  declarations: [
+    LoginComponent
+  ],
+  exports: [
+    LoginComponent
+  ],
 })
 export class UsersModule {}
