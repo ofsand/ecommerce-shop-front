86a713a0352d2ff8973c3b5b4149b18d10c150d8
ofsand <zesoufian@gmail.com>
2022-09-14 16:52:14 +0100

    Deleting the Charts parts

--- apps/admin/src/app/pages/dashboard/dashboard.component.ts
+++ apps/admin/src/app/pages/dashboard/dashboard.component.ts
@@ -14,83 +14,18 @@ export class DashboardComponent implements OnInit {
   usersCount: number;
   productsCount: number;
   totalSales: number;
-  
-  basicData: any;
-  data: any;
-  ordersDates = [];
-  ordersTotals = []
 
-  pipe = new DatePipe('en-US');
-  
   constructor(
     private ordersService: OrdersService,
     private usersService: UsersService,
     private productsService: ProductsService
-  ) { }
-
-  ngOnInit(): void {
-    this._getOrdersCount();
-    this._getUsersCount();
-    this._getProductsCount();
-    this._getTotalSales();
+    ) { }
     
-    this.ordersService.getOrders().subscribe( orders => {
-      //orders.forEach(order => order.dateOrdered)
-      const dates: Date[] = [];
-      orders.map(order => dates.push(order.dateOrdered));
-      dates.forEach( d => this.ordersDates.push(this.pipe.transform(d, 'shortDate')));
-      console.log(this.ordersDates.reverse());
-      orders.map(order => this.ordersTotals.push(order.totalPrice));
-      console.log(this.ordersTotals)
-    })
-    //
-    this.basicData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-          {
-              label: 'First Dataset',
-              data: [65, 59, 80, 81, 56, 55, 40],
-              fill: false,
-              borderColor: '#42A5F5',
-              tension: .4
-          },
-          {
-              label: 'Second Dataset',
-              data: [28, 48, 40, 19, 86, 27, 90],
-              fill: false,
-              borderColor: '#FFA726',
-              tension: .4
-          }
-      ]
-  };
-  //
-  this.data = {
-    datasets: [{
-        data: [
-            11,
-            16,
-            7,
-            3,
-            14
-        ],
-        backgroundColor: [
-            "#42A5F5",
-            "#66BB6A",
-            "#FFA726",
-            "#26C6DA",
-            "#7E57C2"
-        ],
-        label: 'My dataset'
-    }],
-    labels: [
-        "Red",
-        "Green",
-        "Yellow",
-        "Grey",
-        "Blue"
-    ]
-};
-
+    ngOnInit(): void {
+      this._getOrdersCount();
+      this._getUsersCount();
+      this._getProductsCount();
+      this._getTotalSales();
   }
 
   _getUsersCount() {
@@ -116,4 +51,6 @@ export class DashboardComponent implements OnInit {
       this.totalSales = totalSales;
     })
   }
+
+
 }
